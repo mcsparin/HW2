0b9f3c92d207e6f2143ff3e3bc5e77800692b6e1
mcsparin <mcsparin@colorado.edu>
2014-02-28 16:32:37 -0700

    error fix

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,7 @@ module Rottenpotatoes
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
+    config assets.initialize_on_precompile= false
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
